2ada885313d9bca78c30fbeb7b13b50794ed4702
randy8642 <randy8642@gmail.com>
2021-10-14 22:45:51 +0800

    20211014

--- functions.py
+++ functions.py
@@ -14,68 +14,89 @@ def readImages(folderPath:str) -> list:
     fileList = sorted([f for f in os.listdir(folderPath) if f.endswith('jpg')])
     return [cv2.imread(os.path.join(folderPath, file)) for file in fileList]
 
-def conv(arr: np.ndarray):
-
-
-
-    return 0
-
 def erosion(img: np.ndarray, kernal: np.ndarray):
     assert len(img.shape) == 2, '輸入需為二值化圖像'
     assert (kernal.shape[0] % 2 == 1) & (kernal.shape[1] % 2 == 1), 'kernel需為奇數'
     
-    # padding
-    pad_x = kernal.shape[0] // 2
-    pad_y = kernal.shape[1] // 2
-    padImg = np.zeros([img.shape[0] + pad_x*2, img.shape[1] + pad_y*2], dtype=np.int8)
-    padImg[pad_x:-pad_x, pad_y:-pad_y] = img
+
+    res = conv(img, kernal).astype(np.uint8)
+
+    res[res < len(kernal.flatten())] = 0
+    res[res >= len(kernal.flatten())] = 1
     
-    newImg = np.zeros_like(img, dtype=np.int32)
-    # conv
-    for x, y in itertools.product(range(padImg.shape[0] - kernal.shape[0] + 1), range(padImg.shape[1] - kernal.shape[1] + 1)):             
-        newImg[x, y] = np.sum(padImg[x:x+kernal.shape[0], y:y+kernal.shape[1]] * kernal)
-    
-    result = np.zeros_like(img, dtype=np.int8)
-    result[newImg >= 9] = 1
-   
-    return result
+
+    return res
 
 
 def dilation(img: np.ndarray, kernal: np.ndarray):
+    m, n = kernal.shape
+
     assert len(img.shape) == 2, '輸入需為二值化圖像'
-    assert (kernal.shape[0] % 2 == 1) & (kernal.shape[1] % 2 == 1), 'kernel需為奇數'
+    assert (m % 2 == 1) & (n % 2 == 1), 'kernel需為奇數'
 
-    # padding
-    pad_x = kernal.shape[0] // 2
-    pad_y = kernal.shape[1] // 2
-    newImg = np.zeros([img.shape[0] + pad_x*2, img.shape[1] + pad_y*2], dtype=np.int8)
-    newImg[pad_x:-pad_x, pad_y:-pad_y] = img
     
-    # conv
-    for x, y in itertools.product(range(img.shape[0]), range(img.shape[1])):  
-        if img[x, y]:            
-            newImg[x:x+pad_x*2+1, y:y+pad_y*2+1] = np.logical_or(kernal, newImg[x:x+pad_x*2+1, y:y+pad_y*2+1])
 
-    return newImg[pad_x:-pad_x, pad_y:-pad_y]
+    f_img = img.reshape(-1, 1)
+    b = f_img * kernal.flatten()   
+    b = b.reshape(-1, m, n).astype(np.uint8)
 
-def conv(x: np.ndarray, y: np.ndarray):
-    fr = fft2(x)
-    fr2 = fft2(np.flipud(np.fliplr(y)))
-    m,n = fr.shape
-    cc = np.real(ifft2(fr*fr2))
-    cc = np.roll(cc, -m/2+1,axis=0)
-    cc = np.roll(cc, -n/2+1,axis=1)
-    return cc
+    res = np.zeros([img.shape[0] + m//2 *2, img.shape[1] + n//2 *2], dtype=np.uint8)
+    for n, (i, j) in enumerate(itertools.product(range(res.shape[0]-m+1), range(res.shape[1]-n+1 ))):     
+        res[i:i+kernal.shape[0], j:j+kernal.shape[1]] = np.logical_or(b[n], res[i:i+kernal.shape[0], j:j+kernal.shape[1]])
+
+    return res
 
 
-# img = np.ones([10, 10], dtype=np.int8)
-# img[5, 5] = 0
+def conv(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    # pad
+    pad = np.array(y.shape) // 2
+    padded_x = np.zeros([x.shape[0] + pad[0]*2, x.shape[1] + pad[1]*2])
+    padded_x[pad[0]:-pad[0], pad[1]:-pad[1]] = x
+    
 
-# kernel = [[1, 1, 1],
-#           [1, 1, 1],
-#           [1, 1, 1]]
+    # conv windows
+    # divide the matrix into sub_matrices of kernel size
+    view_shape = tuple(np.subtract(padded_x.shape, y.shape) + 1) + y.shape
+    strides = padded_x.strides + padded_x.strides
+    sub_matrices = np.lib.stride_tricks.as_strided(padded_x, view_shape, strides)
+    # convert non_zero elements to 1 (dummy representation)
+    # sub_matrices[sub_matrices > 0.] = 1.
+    
+    m = np.einsum('ij,klij->kl', y, sub_matrices)
 
-# kernel = np.array(kernel)
-# print(img)
-# a = erosion(img, kernel)
-# print(a)
+    return m
+
+def connectedComponents(x:np.ndarray):
+    labels = [0]
+    connected = [0]
+    flag = np.zeros_like(x, dtype=np.int32)
+    for n, (i, j) in enumerate(itertools.product(range(x.shape[0]), range(x.shape[1]))):
+        if x[i, j] == 0:
+            continue
+       
+        if (i > 0) and (j > 0) and (flag[i-1, j] != 0) and (flag[i, j-1] != 0):
+            connected[flag[i, j-1]] = flag[i-1, j]
+            flag[i, j] = flag[i-1, j]
+        elif ((flag[i-1, j] != 0) and (i > 0)) and ((flag[i, j-1] == 0) or (j == 0)):
+            flag[i, j] = flag[i-1, j]
+        elif ((flag[i-1, j] == 0) or (i == 0)) and ((flag[i, j-1] != 0) and (j > 0)):
+            flag[i, j] = flag[i, j-1]
+        else:
+            labels.append(labels[-1] + 1)
+            connected.append(labels[-1])
+            flag[i, j] = labels[-1]
+
+    for l in range(1, len(labels)):
+        c = l
+        while c != connected[c]:
+            c = connected[c]
+        flag[flag == l] = c
+
+    return flag
+
+def ad(x:np.ndarray):
+    # https://cloud.tencent.com/developer/ask/72570
+
+
+
+    return x
--- main.py
+++ main.py
@@ -2,26 +2,47 @@ import argparse
 import functions
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import convolve2d
 
 def main():
     img = functions.readImages('./sourceImages')[0]
     grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   
+    hist, bin_edges = np.histogram(grayImg.flatten(), bins=range(256), density=True)
+    newScale = 255 * np.cumsum(hist)
+    newScale = np.concatenate([newScale, [newScale[-1]]], axis=0)
+    newScale = newScale.astype(np.uint8)
 
-    # cv2.imshow('gray', grayImg)
-    # cv2.waitKey(0)
+    img = newScale[grayImg]
+    
+    img = functions.conv(img, np.ones([11, 11])/121).astype(np.uint8)
+    
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 199, 5)
 
-    kernel = [[1, 1, 1],
-              [1, 1, 1],
-              [1, 1, 1],]
+    img = functions.dilation(img, np.ones([3, 3]))
 
-    eImg = functions.erosion(grayImg, np.array(kernel))
-    closingImg = functions.dilation(eImg, np.array(kernel))
+    img = functions.erosion(img, np.ones([5, 5]))
 
-    cv2.imshow('close', cv2.resize(closingImg.astype(np.uint8), (800, int(800/closingImg.shape[1]*closingImg.shape[0]))))
-    cv2.waitKey(0)
+    # num_labels, labels = cv2.connectedComponents(img, connectivity=4)
+    labels = functions.connectedComponents(img)
+    
+    uni, cnt = np.unique(labels, return_counts=True)
 
-    pass
+    tmp = np.zeros_like(img, dtype=np.uint8)
+    for u, c in zip(uni[1:], cnt[1:]):
+        if c > 300:
+            tmp[labels == u] = 255
+    img = tmp
 
+    img = functions.dilation(img, np.ones([5, 5]))
+    showImg(img)
+
+
+def showImg(img):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(img, cmap='gray')
+    plt.show()
 
 if __name__=='__main__':
     main()
