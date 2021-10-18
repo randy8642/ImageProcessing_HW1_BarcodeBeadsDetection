9863077859a75b8a2eacff8b5ae0d51e9faab6d9
randy8642 <randy8642@gmail.com>
2021-10-18 21:48:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ import matplotlib.pyplot as plt
 def main(srcPath, desPath):
     img = cv2.imread(srcPath)
     
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    # convert to grayscale
+    img = img[:, :, 2]*0.299 + img[:, :, 1]*0.587 + img[:, :, 0]*0.114
+    img = img.astype(np.uint8)
+    
+    # transfer to binary image by adaptive threshold method
     img = functions.adaptiveThreshold(img, kernalSize = 111, offset=-3)
+
+    # run erosion (7*7 kernel used)
     img = functions.erosion(img, np.ones([7, 7]))
+
+    # run dilation 3 times (7*7 kernel used)
     for _ in range(3):
         img = functions.dilation(img, np.ones([7, 7]))
     
-
+    # CCL
     labels = functions.connectedComponents(img)
     uni, cnt = np.unique(labels, return_counts=True)
     tmp = np.zeros(img.shape, dtype=np.uint8)
@@ -23,8 +30,13 @@ def main(srcPath, desPath):
     tmp[labels == uni[np.argmax(cnt[1:]) + 1]] = 0
     img = tmp
 
-    img = np.logical_not(img).astype(np.uint8)*255
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    # reverse black and white
+    img = np.logical_not(img).astype(np.uint8) * 255
+
+    # convert grayscale to RGB
+    img = np.concatenate([np.expand_dims(img, axis=-1)] * 3, axis=-1)
+
+    # save image to disk
     cv2.imwrite(desPath, img)
     
 
@@ -42,4 +54,3 @@ if __name__=='__main__':
 
 
     main(src, des)
-    main('./sourceImages/W_A1_0_3.jpg', './resultImages/W_A1_0_3.jpg')
